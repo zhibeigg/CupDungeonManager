25399b8d822bc7b93dfb333c5b16e5cd3aef9b56
zhibei <785740487@qq.com>
2023-04-02 22:44:09 +0800

    更新复活币

--- src/main/kotlin/com/github/cupdungeonmanager/all/factory/count/CountManager.kt
+++ src/main/kotlin/com/github/cupdungeonmanager/all/factory/count/CountManager.kt
@@ -1,9 +1,13 @@
 package com.github.cupdungeonmanager.all.factory.count
 
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.event.player.PlayerGameModeChangeEvent
+import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.serverct.ersha.dungeon.DungeonPlus
 import taboolib.common.platform.event.SubscribeEvent
+import taboolib.platform.util.getMeta
+import taboolib.platform.util.sendLang
 
 object CountManager {
 
@@ -17,6 +21,22 @@ object CountManager {
                 val ui = CountUI(player)
                 ui.open()
                 ui.mubei()
+            }
+        }
+    }
+
+    @SubscribeEvent
+    fun e(e: PlayerInteractEntityEvent) {
+        val player = e.player
+        val world = player.world
+        val manager = DungeonPlus.dungeonManager
+        if (manager.isDungeonWorld(world)) {
+            val team = e.rightClicked.getMeta("team").toString()
+            val death = Bukkit.getPlayerExact(team) ?: return
+            death.gameMode = GameMode.SURVIVAL
+            death.teleport(e.rightClicked)
+            DungeonPlus.teamManager.getTeam(player)?.players?.forEach {
+                Bukkit.getPlayer(it)?.sendLang("team-help-other", death.displayName, player.displayName)
             }
         }
     }
