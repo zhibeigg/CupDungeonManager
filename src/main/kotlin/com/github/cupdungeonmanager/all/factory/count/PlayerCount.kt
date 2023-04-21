eab8381c4d990dadc1fc24851e5af0f14ab9e532
zhibei <785740487@qq.com>
2023-04-21 19:13:44 +0800

    修复

--- src/main/kotlin/com/github/cupdungeonmanager/all/factory/count/PlayerCount.kt
+++ src/main/kotlin/com/github/cupdungeonmanager/all/factory/count/PlayerCount.kt
@@ -3,35 +3,28 @@ package com.github.cupdungeonmanager.all.factory.count
 import com.github.cupdungeonmanager.all.api.events.player.PlayerCountEvents
 import com.github.cupdungeonmanager.all.storage.Storage
 import org.bukkit.entity.Player
-import taboolib.common.platform.function.submitAsync
 
 class PlayerCount(val player: Player) {
 
     fun add(number: Int) {
         PlayerCountEvents.Add(player, number).call()
-        submitAsync {
-            val now = Storage.INSTANCE.getCount(player)
-            Storage.INSTANCE.setCount(player, now + number)
-        }
+        val now = Storage.INSTANCE.getCount(player)
+        Storage.INSTANCE.setCount(player, now + number)
     }
 
     fun reduce(number: Int) {
         PlayerCountEvents.Reduce(player, number).call()
-        submitAsync {
-            val now = Storage.INSTANCE.getCount(player)
-            if (now <= number) {
-                Storage.INSTANCE.setCount(player, 0)
-            } else {
-                Storage.INSTANCE.setCount(player, now - number)
-            }
+        val now = Storage.INSTANCE.getCount(player)
+        if (now <= number) {
+            Storage.INSTANCE.setCount(player, 0)
+        } else {
+            Storage.INSTANCE.setCount(player, now - number)
         }
     }
 
     fun set(number: Int) {
         PlayerCountEvents.Set(player, number).call()
-        submitAsync {
-            Storage.INSTANCE.setCount(player, number)
-        }
+        Storage.INSTANCE.setCount(player, number)
     }
 
     fun get() : Int {
--- src/main/kotlin/com/github/cupdungeonmanager/all/factory/limit/PlayerLimit.kt
+++ src/main/kotlin/com/github/cupdungeonmanager/all/factory/limit/PlayerLimit.kt
@@ -3,50 +3,39 @@ package com.github.cupdungeonmanager.all.factory.limit
 import com.github.cupdungeonmanager.all.api.events.player.PlayerLimitEvents
 import com.github.cupdungeonmanager.all.storage.Storage
 import org.bukkit.entity.Player
-import taboolib.common.platform.function.submitAsync
 
 class PlayerLimit(val player: Player) {
 
 
     fun reset() {
         PlayerLimitEvents.Clear(player, null).call()
-        submitAsync {
-            Storage.INSTANCE.resetLimit(player)
-        }
+        Storage.INSTANCE.resetLimit(player)
     }
 
     fun add(number: Int, dungeon: String) {
         PlayerLimitEvents.Add(player, number, dungeon).call()
-        submitAsync {
             val now = Storage.INSTANCE.getLimit(player, dungeon)
             Storage.INSTANCE.setLimit(player, now + number, dungeon)
-        }
     }
 
     fun reduce(number: Int, dungeon: String) {
         PlayerLimitEvents.Reduce(player, number, dungeon).call()
-        submitAsync {
-            val now = Storage.INSTANCE.getLimit(player, dungeon)
-            if (now <= number) {
-                Storage.INSTANCE.setLimit(player, 0, dungeon)
-            } else {
-                Storage.INSTANCE.setLimit(player, now - number, dungeon)
-            }
+        val now = Storage.INSTANCE.getLimit(player, dungeon)
+        if (now <= number) {
+            Storage.INSTANCE.setLimit(player, 0, dungeon)
+        } else {
+            Storage.INSTANCE.setLimit(player, now - number, dungeon)
         }
     }
 
     fun set(number: Int, dungeon: String) {
         PlayerLimitEvents.Set(player, number, dungeon).call()
-        submitAsync {
-            Storage.INSTANCE.setLimit(player, number, dungeon)
-        }
+        Storage.INSTANCE.setLimit(player, number, dungeon)
     }
 
     fun clear(dungeon: String) {
         PlayerLimitEvents.Clear(player, dungeon).call()
-        submitAsync {
-            Storage.INSTANCE.clearLimit(player, dungeon)
-        }
+        Storage.INSTANCE.clearLimit(player, dungeon)
     }
 
 }
